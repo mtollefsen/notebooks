2ab92549a5dc5102d2c9a72d84c8ce5fbda012b8
Max T <97869630+SkeletorsBiceps@users.noreply.github.com>
2022-02-08 15:06:18 -0600

    Update match_map_stats CSV Cleanup.sql

--- match_map_stats CSV Cleanup.sql
+++ match_map_stats CSV Cleanup.sql
@@ -1,5 +1,5 @@
 
--- Corrects column misspellings
+-- Corrects column misspellings, "perecent" -> "percent"
       ALTER TABLE match_map_stats
            RENAME attacker_control_perecent TO attacker_control_percent;
            
@@ -15,6 +15,19 @@
                                   END);
 
 
--- Renames "control_round_name" to "map_subsection
+-- Renames "control_round_name" to "map_subsection"
       ALTER TABLE match_map_stats
            RENAME control_round_name TO map_subsection;
+
+-- Corrects map_subsection of match 21352, game 1, round 1
+UPDATE match_map_stats
+   SET map_subsection  = "MEKA Base"
+ WHERE ROWID = 4065;
+
+
+-- Creates a best estimate of round_start_time for match 21352, game 1, round 1
+UPDATE match_map_stats
+   SET round_start_time = (SELECT datetime((strftime("%s", round_end_time) - 215), "unixepoch")
+                             FROM match_map_stats
+                            WHERE ROWID = 4065)
+ WHERE ROWID = 4065;
