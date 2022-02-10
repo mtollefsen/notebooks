79202cdb3e2c11be32b2b4c185321915c8b06c74
Max T <97869630+SkeletorsBiceps@users.noreply.github.com>
2022-02-10 16:07:38 -0600

    Update match_map_stats CSV Cleanup.sql

--- match_map_stats CSV Cleanup.sql
+++ match_map_stats CSV Cleanup.sql
@@ -7,12 +7,23 @@
            RENAME defender_control_perecent TO defender_control_percent;
 
 
--- Creates match_loser column
+-- Creates "match_loser" column
       ALTER TABLE match_map_stats
-       ADD COLUMN match_loser AS (CASE
+       ADD COLUMN match_loser;
+
+-- Populates "match_loser" column
+           UPDATE match_map_stats
+              SET match_loser = (CASE
                                      WHEN match_winner = team_one_name THEN team_two_name
                                      ELSE team_one_name
                                   END);
+
+-- Removes columns "team_one_name" and "team_two_name"
+      ALTER TABLE match_map_stats
+      DROP COLUMN team_one_name;
+
+      ALTER TABLE match_map_stats
+      DROP COLUMN team_two_name;
 
 
 -- Renames "control_round_name" to "map_subsection"
