ca6bef86e982ef13efb1dd607aa535b78852ca2a
Max T <97869630+SkeletorsBiceps@users.noreply.github.com>
2022-02-12 22:44:37 -0600

    Update 4. Create New Tables.sql

--- 4. Create New Tables.sql
+++ 4. Create New Tables.sql
@@ -1,22 +1,22 @@
         CREATE TABLE game (
-                          start_time TEXT,
-			  match_id TEXT,
-			  game_id TEXT PRIMARY KEY,
-			  game_number INTEGER,
-			  winner TEXT,
-			  loser TEXT,
-			  map_name TEXT,
-			  game_mode TEXT,
-			  winning_score INTEGER,
-			  losing_score INTEGER
-			  );
+                     start_time TEXT,
+	  	     match_id TEXT,
+		     game_id TEXT PRIMARY KEY,
+	             game_number INTEGER,
+          	     winner TEXT,
+		     loser TEXT,
+	             map_name TEXT,
+		     game_mode TEXT,
+		     winning_score INTEGER,
+	             losing_score INTEGER
+	             );
 
 
          INSERT INTO game
               SELECT DISTINCT 
                      mms.round_start_time as start_time,
 	             mms.match_id,
-                     mms.match_id || '.' || mms.game_number as game_id,
+                     mms.match_id || "." || mms.game_number as game_id,
 	             mms.game_number,
 	             mms.map_winner as winner,
 	             mms.map_loser as loser,
