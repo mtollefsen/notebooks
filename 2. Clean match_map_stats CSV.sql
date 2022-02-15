cc7504eae8cf1d8a767f6eb61f92b1f877485771
Max T <97869630+maxtoll@users.noreply.github.com>
2022-02-14 20:56:10 -0600

    Update 2. Clean match_map_stats CSV.sql

--- 2. Clean match_map_stats CSV.sql
+++ 2. Clean match_map_stats CSV.sql
@@ -1,18 +1,13 @@
 
--- Corrects column misspellings, "perecent" -> "percent"
       ALTER TABLE match_map_stats
            RENAME attacker_control_perecent TO attacker_control_percent;
            
       ALTER TABLE match_map_stats
            RENAME defender_control_perecent TO defender_control_percent;
 
-
--- Creates "match_loser" column
       ALTER TABLE match_map_stats
        ADD COLUMN match_loser;
-
-
--- Populates "match_loser" column
+       
            UPDATE match_map_stats
               SET match_loser = (CASE
                                    WHEN match_winner = team_one_name 
@@ -20,8 +15,6 @@
                                    ELSE team_one_name
                                  END);
 
-
--- Removes columns "team_one_name" and "team_two_name"
       ALTER TABLE match_map_stats
       DROP COLUMN team_one_name;
 
@@ -29,18 +22,17 @@
       DROP COLUMN team_two_name;
 
 
--- Renames "control_round_name" to "map_subsection"
       ALTER TABLE match_map_stats
            RENAME control_round_name TO map_subsection;
 
 
--- Corrects map_subsection of match 21352, game 1, round 1
+--Corrects map_subsection of match 21352, game 1, round 1
            UPDATE match_map_stats
               SET map_subsection  = "MEKA Base"
             WHERE ROWID = 4065;
 
 
--- Creates an estimate (based off game footage) of round_start_time for match 21352, game 1, round 1
+--Creates an estimate (based off game footage) of round_start_time for match 21352, game 1, round 1
            UPDATE match_map_stats
               SET round_start_time = (SELECT datetime((strftime("%s", round_end_time) - 215), "unixepoch")
                                         FROM match_map_stats
@@ -48,7 +40,7 @@
             WHERE ROWID = 4065;
  
  
--- Each query deletes a duplicate row (some with incorrect round_start_time)
+--Each query deletes a duplicate row (some with incorrect round_start_time)
       DELETE FROM match_map_stats
             WHERE ROWID = 4066;
                    
@@ -62,7 +54,7 @@
             WHERE ROWID = 9943;
           
           
--- Each query corrects a game_number
+--Each query corrects a game number
            UPDATE match_map_stats
               SET game_number = "4"
             WHERE ROWID IN (664, 665);
@@ -72,10 +64,10 @@
             WHERE ROWID IN (5364, 5365);
             
             
--- Corrects the round numbers in "map_round"
--- NOTE: This specifically corrects an error where many
--- of the round numbers were increasing in steps of 2 
--- (e.g. "1, 3, 5" instead of "1, 2, 3")
+--Corrects the round numbers in "map_round"
+--NOTE: This specifically corrects an error where many
+--of the round numbers were increasing in steps of 2 
+--(e.g. "1, 3, 5" instead of "1, 2, 3")
              WITH
  map_round_lag AS (
            SELECT round_start_time,
@@ -103,13 +95,11 @@
               FROM map_round_new mrn
              WHERE mrn.round_start_time = match_map_stats.round_start_time;
              
-	     
--- Renames "map_round" to "round_number"
        ALTER TABLE match_map_stats
 	    RENAME map_round TO round_number;
 
 
--- Formats values in "stage"
+--Formats values in "stage"
             UPDATE match_map_stats
                SET stage = REPLACE(stage, "Overwatch League Inaugural Season Championship", "2018 Playoffs")
              WHERE SUBSTR(round_start_time, 1, 4) = "2018";
