cb363960e90b222a0e5be6855f5ce5b64190ff0a
Max Tollefsen <97869630+mtollefsen@users.noreply.github.com>
2023-10-26 20:09:07 -0500

    Update player_info_web_scrape.py

--- Liquipedia API Web Scrape/player_info_web_scrape.py
+++ Liquipedia API Web Scrape/player_info_web_scrape.py
@@ -4,61 +4,58 @@ import pandas as pd
 import sqlite3
 import time
 
-# Function that prints a 30 second countdown
-# Liquipedia API rules require 30 seconds wait between API requests
+
 def waitThirty():
+    """
+    Prints a 30 second countdown.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     for i in range(30, 0, -1):
         count = str(i).ljust(2, ' ')
         print(f'\r{count}', end='\r', flush=True)
         time.sleep(1)
     print('\r', end='')
+# end of waitThirty function definition
 
+def getPlayerInfo(headers, player):
+    """
+    Retrieves pro player's information from their Liquepedia.net page.
 
-# Getting a list of all players from Overwatch_League.db
-conn = sqlite3.connect(r'URL HERE')
-query = "SELECT DISTINCT player FROM player_stat"
-playerList = conn.execute(query).fetchall()
-playerList = [player[0] for player in playerList]
-conn.close()
+    Takes a professional Overwatch League player's name which is used to makes GET requests to 
+    Liquipedia.net's API which returns HTML that is then parsed for the player's information using 
+    BeautifulSoup. Function will try multiple formats (different capitalization combinations) of the 
+    player's name to make requests with.
 
+    Args:
+        headers: dict with header information for API requests.
+        player: string of player name to make requests for.
 
-headers = {'User-Agent': 'USER AGENT HERE',
-            'Accept-Encoding': 'gzip'}
-masterPlayerData = []
-playerID = 0
-successfulScrapes = 0
-justLength = len(max(playerList, key=len)) + 4
-formatAltCount = 0
-
-
-print('[Starting API Requests]')
-
-# Gathers player's information from Liquipedia and appends it to masterPlayerData
-for player in playerList:
-    playerID += 1
-    ambiguous = False
+    Returns:
+        List with the player's information if the given player's Liquepedia.net page was
+        succesfully found. If not, a string of what happened is returned instead.
+        This string can be:
+            "not found" - for when every player name format was tried with no results
+            "ambiguous" - for when a redirect page with multiple redirects was reached
+    """
     playerFormatCache = []
-    formatAltNeeded = False
-    playerData = [playerID, player]
-    print(player.ljust(justLength, ' '), end='')
-    print(f'({playerID})')
+    for playerNameFormat in [player, 
+                             player.capitalize(), 
+                             player.title(), 
+                             player.upper(), 
+                             player.lower()]:
 
-
-    # Sends requests each with different formatting applied to player name
-    for playerNameFormat in [player, player.capitalize(), player.title(), 
-                              player.upper(), player.lower()]:
-        player = playerNameFormat
-        # Prevents repeat requests
-        if player in playerFormatCache:
+        # check if we've already made a request with this player name format (prevents repeat requests)
+        if playerNameFormat in playerFormatCache:
             continue
-        else:
-            playerFormatCache.append(player)
-            if len(playerFormatCache) > 1:
-                formatAltNeeded = True
 
-
-        print(('  ' + player).ljust(justLength, '-'), end='')
-        url = r'https://liquipedia.net/overwatch/api.php?action=parse&format=json&page=' + player
+        # make initial request
+        print(('  ' + playerNameFormat).ljust(justLength, '-'), end='')
+        url = r'https://liquipedia.net/overwatch/api.php?action=parse&format=json&page=' + playerNameFormat
         try:
             response = requests.get(url, headers=headers)
         except:
@@ -66,39 +63,41 @@ for player in playerList:
             waitThirty()
             continue
 
+        # if parse in initial response from request
         if 'parse' in response.json().keys():
+            # try to parse initial response
             try:
                 presoup = response.json()['parse']['text']['*']
                 soup = BeautifulSoup(presoup, 'html5lib')
             except KeyError:
                 try:
-                    print(response.json()['error']['info'])
+                    print(response.json()['error']['info'])   # try to print error info from HTML that was scraped
                 except KeyError:
                     print('failed to parse response')
                 finally:
                     waitThirty()
                     continue
 
-            # Checking for player info, if none then check for a redirect page
+            # if there is no player info
             if soup.select('.infobox-cell-2') == []:
                 try:
-                    # Checking if there are multiple redirect links
+                    # Check if there are multiple redirect links
                     if len(response.json()['parse']['links']) > 1:
-                        print('Player name has ambiguity!')
-                        playerData += (['ambiguous'] * 4)
-                        masterPlayerData.append(playerData)
-                        ambiguous = True
-                        break
-                    player = response.json()['parse']['links'][0]['*']    # alternative: player = soup.ul.a['title']
+                        return "ambiguous"
+                    # only one redirect link, grab the page name
+                    else:
+                        redirectPage = response.json()['parse']['links'][0]['*']    # alternative line of code: player = soup.ul.a['title']
                 except (ValueError, KeyError):
                     print('could not find redirect')
                     waitThirty()
                     continue
                         
+                # make request for the page we were redirected to
                 print('redirect found')
-                time.sleep(30)
-                url = r'https://liquipedia.net/overwatch/api.php?action=parse&format=json&page=' + player
-                print(('  '+player).ljust(justLength, '-'), end='')
+                waitThirty()
+                url = r'https://liquipedia.net/overwatch/api.php?action=parse&format=json&page=' + redirectPage
+                print(('  '+redirectPage).ljust(justLength, '-'), end='')
+                # check response
                 try:
                     response = requests.get(url, headers=headers)
                 except:
@@ -106,54 +105,95 @@ for player in playerList:
                     waitThirty()
                     continue
 
+                # try to parse redirect page
                 try:
                     presoup = response.json()['parse']['text']['*']
                     soup = BeautifulSoup(presoup, 'html5lib')
                 except KeyError:
                     try:
-                        print(response.json()['error']['info'])
+                        print(response.json()['error']['info'])   # try to print error info from HTML that was scraped
                     except KeyError:
                         print('failed to parse redirected response')
                     finally:
                         waitThirty()
                         continue
                 
+                # check if there is player info at the redirect page
                 if soup.select('.infobox-cell-2') == []:
                     print('redirect doesn\'t have player info')
                     waitThirty()
                     continue
+                # player info found
                 else:
-                    break
+                    return [i.text for i in list(soup.select('.infobox-cell-2'))]
                 
-                
+            # player info found
             else:
-                break
+                return [i.text for i in list(soup.select('.infobox-cell-2'))]
             
-            
+        # parsing initial request failed   
         else:
             try:
-                print(response.json()['error']['info'])
+                print(response.json()['error']['info'])   # try to print error info from HTML that was scraped
             except KeyError:
                 print('failed to parse response')
             finally:
                 waitThirty()
-                continue
+                continue   # return to main for loop and try requests for another player's name
             
-            
-    else:
+    # outside for loop,
+    # every format of the player's name was tried without finding their webpage     
+    return "not found"
+# End of getPlayerInfo function definition
+
+
+# Start of script =======================================================================================================
+# Get the list of all pro players in Overwatch_League.db (my personal database)
+conn = sqlite3.connect(r'FILE PATH TO DATABASE HERE')
+query = "SELECT DISTINCT player FROM player_stat"
+playerList = conn.execute(query).fetchall()
+playerList = [player[0] for player in playerList]
+conn.close()
+
+# set variable values
+headers = {'User-Agent': 'USER AGENT NAME HERE',  # header for API calls
+            'Accept-Encoding': 'gzip'}
+masterPlayerData = []  # list where we'll put all player data into
+playerID = 0
+successfulScrapes = 0
+ambiguousPlayerNames = 0
+formatAltCount = 0
+justLength = len(max(playerList, key=len)) + 4   # justification length for nice printing
+
+print('[Starting API Requests]')
+
+# Main for loop, gather player's information from Liquipedia.net and append it to masterPlayerData
+for player in playerList:
+    playerID += 1
+    formatAltNeeded = False
+    playerData = [playerID, player]
+    print(player.ljust(justLength, ' '), end='')
+    print(f'({playerID})')
+
+    # call getPlayerInfo
+    playerInfo = getPlayerInfo(headers, player)
+    
+    # check for error strings
+    if playerInfo == "not found":
         print('  Could not find page!')
         playerData += (['could not find page'] * 4)
         masterPlayerData.append(playerData)
         waitThirty()
         continue
-    
-    if ambiguous == True:
+    elif playerInfo == "ambiguous":
+        print('Player name has ambiguity!')
+        playerData += (['ambiguous'] * 4)
+        masterPlayerData.append(playerData)
+        ambiguousPlayerNames += 1
+        waitThirty()
         continue
 
-    # Player's page was found successfully
-    # Creates list of player info from our get request
-    playerInfo = [i.text for i in list(soup.select('.infobox-cell-2'))]
-    
+    # Player's page was found successfully, add info to masterPlayerData
     # Add real name
     try:
         nameIndex = playerInfo.index('Name:') + 1
@@ -162,7 +202,7 @@ for player in playerList:
     else:
         playerData.append(playerInfo[nameIndex])
        
-    # Add romanized name
+    # Add Romanized name
     try:
         rnameIndex = playerInfo.index('Romanized Name:') + 1
     except ValueError:
@@ -202,15 +242,16 @@ for player in playerList:
     waitThirty()
     continue
 
-# All API requests finished, printing stats
+# All API requests finished ==============================================================================================
+# print stats
 print('[Finished with API Requests]')
 print()
-print(f'{playerID} scrape attempts')
+print(f'{playerID} total scrape attempts made')
 print(f'{successfulScrapes} successful scrapes')
-print(f'{formatAltCount} times an alt format was needed')
-
+print(f'{ambiguousPlayerNames} ambiguous player names')
 
 # Manually add info for players not found by the above API requests
+# (this was added after running the script once and seeing which players the script was unable to get information for)
 masterPlayerData[125][1:] = ['Choihyobin', '최효빈', 'Choi Hyo-bin', '1997-09-05', 'South Korea']
 masterPlayerData[128][1:] = ['Snow', 'Mikias Yohannes', 'n/a', 'n/a', 'Ethiopia United States']
 masterPlayerData[160][1:] = ['BEBE', '윤희창', 'Yoon Hee-chang', '1999-02-03', 'South Korea']
@@ -221,13 +262,11 @@ masterPlayerData[270][1:] = ['blase', 'Jeffrey Tsang', 'n/a', '1999-02-22', 'Uni
 masterPlayerData[276][1:] = ['Ttuba', '이호성', 'Lee Ho-sung', '1999-09-21', 'South Korea']
 masterPlayerData[332][1:] = ['Hybrid', 'Dominic Grove', 'n/a', '2001-03-28', 'United Kingdom']
 
-
-# Putting player information into a pandas DataFrame
+# Place player information into a pandas DataFrame
 columns = ['player_id', 'pro_name', 'real_name', 'romanized_name', 'birthday', 'country']
 df = pd.DataFrame(masterPlayerData, columns=columns)
 
-
-# Exports DataFrame of player info to SQL database
+# Export DataFrame of player info to SQL database
 conn = sqlite3.connect(r'URL HERE')
 df.to_sql('player', con=conn, index=False)
 conn.close()
